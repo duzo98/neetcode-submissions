02aa5c3fb80060489770d1b359627a993ca4b5e5
Uzo <160682662+duzo98@users.noreply.github.com>
2026-09-24 22:39:25 -0500

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,22 @@
+
+variable_int_type = 10
+variable_float_type = 25.2
+variable_bool_type = True
+variable_str_type = "Dan"
+variable_list_type = [1,2,3]
+
+
+
+
+
+
+
+print(type(variable_int_type))
+
+print(type(variable_float_type))
+
+print(type(variable_bool_type))
+
+print(type(variable_str_type))
+
+print(type(variable_list_type))
